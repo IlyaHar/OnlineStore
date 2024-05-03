4ed608d26d4bf594160d65f50573a3f4d4074417
IlyaHar <harcenkoila278@gmail.com>
2024-05-03 09:01:38 +0300

    added home and contact pages

---
+++ app/views/404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ошибка - 404</title>
+    <meta name="description" content="Главная страница сайта" />
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="/public/css/main.css" type="text/css" charset="utf-8">
+</head>
+<body>
+<div class="container">
+    <h1>Страница ошибки 404</h1><br>
+    <a href="/home">На главную</a>
+</div>
+</body>
+</html>
